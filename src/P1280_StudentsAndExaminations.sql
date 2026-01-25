e5e26396fc53f068406943d46ebafba418b46d3b
saulms <zhsamuel11@gmail.com>
2026-01-24 20:37:18 -0500

    Problem 1280

--- src/P1280_StudentsAndExaminations.sql
+++ src/P1280_StudentsAndExaminations.sql
@@ -1,12 +1,12 @@
 SELECT
-    Students.student_id,
-    Students.student_name,
-    Subjects.subject_name,
-    IFNULL(E.c, 0) attended_exams
-FROM Students JOIN Subjects ON student_id
-LEFT JOIN (
-    SELECT student_id, subject_name, COUNT(*) c
-    FROM Examinations
-    GROUP BY student_id, subject_name
-) E USING (student_id, subject_name)
+    S.student_id,
+    S.student_name,
+    U.subject_name,
+    COALESCE(E.attended, 0) attended_exams
+FROM Students S CROSS JOIN Subjects U
+    LEFT JOIN (
+        SELECT *, COUNT(*) attended
+        FROM Examinations
+        GROUP BY student_id, subject_name
+    ) E USING (student_id, subject_name)
 ORDER BY student_id, subject_name;
